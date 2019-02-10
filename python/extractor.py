4e44ae83df2e0bbadbb3457e2a928fedf77ebf1b
Unknown <jorisberinger@gmail.com>
2019-02-10 17:29:34 +0100

    add types

--- python/extractor.py
+++ python/extractor.py
@@ -4,7 +4,7 @@ import logging
 
 logger = logging.getLogger(__name__)
 
-def getEvents(data):
+def getEvents(data: pd.DataFrame) -> pd.Series:
     events = pd.Series()
     basal = data[data['basalValue'].notnull()]
     if not basal.empty:
@@ -12,25 +12,20 @@ def getEvents(data):
             lambda event:  Event.createTemp(time=event.name, dbdt=event['basalValue'], t1=event['date']+','+event['time'], t2=event['date']+','+event['time']), axis=1)
         events = events.append(basalEvents)
 
-    #basalEvents.apply(lambda event: event.toString(), 1)
-
     bolus = data[data['bolusValue'].notnull()]
     if not bolus.empty:
         bolusEvents = bolus.apply(
             lambda event: Event.createBolus(time=event.name, units=event['bolusValue']), axis=1)
         events = events.append(bolusEvents)
-    #bolusEvents.apply(lambda event: event.toString(), 1)
 
     meal = data[data['mealValue'].notnull()]
     if not meal.empty:
         mealEvents = meal.apply(
             lambda event: Event.createCarb(time=event.name, grams=event['mealValue'], ctype=3*60), axis=1) # TODO Carbtype
         events = events.append(mealEvents)
-    #mealEvents.apply(lambda event: event.toString(), 1)
-
-    # return pd.Series([basalEvents, bolusEvents, mealEvents], index=["basal", "bolus", "meal"])
 
     return events
+
 
 def getBGContinious(data):
     cgm = data[data['cgmValue'].notnull()]
