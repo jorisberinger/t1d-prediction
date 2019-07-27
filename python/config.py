da7645d881021766f9b9dce662aff837290dfd0f
Joris Beringer <jorisberinger@gmail.com>
2019-07-27 21:59:12 +0200

    change db name

--- python/config.py
+++ python/config.py
@@ -1,10 +1,10 @@
 PREDICTION_CONFIG = {
-    "runtime_in_minutes": 7 * 60,         # Prediction will stop either after specified runtime or after reaching
-    "max_number_of_results": 2000,     # max number of results, whichever comes first.
+    "runtime_in_minutes": 10 * 60,         # Prediction will stop either after specified runtime or after reaching
+    "max_number_of_results": 1,     # max number of results, whichever comes first.
     "create_plots": False           # Create a plot for every example containing different predictions and events
 }
 
 DATA_CONFIG = {
-    "database_path": 'data/tinydb/dbjsontest1.json',
-    "csv_input_path": "data/csv/csv_27283995.csv"
+    "database_path": 'data/tinydb/db3p.json',
+    "csv_input_path": "data/csv/csv_82923830.csv"
 }
